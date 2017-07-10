29c5de6421092a1b37d15c82198bd99a846b367f
Daniel Griffin <daniel.griffin@aggiemail.usu.edu>
2017-07-10 12:57:53 -0600

    j-docs

--- src/GeneralGame/GameMove.java
+++ src/GeneralGame/GameMove.java
@@ -1,5 +1,9 @@
 package GeneralGame;
 
+/**
+ * @author Daniel
+ * This allows a specific move to be passed around
+ */
 public abstract class GameMove {
 
 }
--- src/GeneralGame/location.java
+++ src/GeneralGame/location.java
@@ -1,6 +1,13 @@
 package GeneralGame;
 
+/**
+ * @author Daniel
+ * This is veriable that represents a specific board location
+ */
 public class location {
+/**
+ * x and y corodanats of a 2 dementional location. x is associated with rows and y with columns
+ */
 public int x,y;
 public location(int x,int y){
 	this.x = x;
